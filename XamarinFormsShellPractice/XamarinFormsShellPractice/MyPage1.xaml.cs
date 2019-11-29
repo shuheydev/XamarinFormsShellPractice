7b0ce08b6d13b95340dead7e8f620c78c5079f2e
shuheydev <shuhey.dev@gmail.com>
2019-11-29 21:02:58 +0900

    Add project files.

---
+++ XamarinFormsShellPractice/XamarinFormsShellPractice/MyPage1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsShellPractice
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyPage1 : ContentPage
+    {
+        public MyPage1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var mp2 = new MyPage2();
+            Shell.SetTabBarIsVisible(mp2, false);
+            await Shell.Current.Navigation.PushAsync(mp2, true);
+        }
+    }
+}
